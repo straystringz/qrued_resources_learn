4eb0d51a04744a957802381b2cb9563a952a3198
ChocoMarissa Qrued <victor.j.emman@gmail.com>
2023-06-13 09:38:55 +0100

    Started working on the BackEnd (app.py) deleted some files too

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
 from flask import Flask, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import input_required, length, ValidationError
 
-app = Flask(__name__)
+
+app = Flask(__name__, static_url_path='/static')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'Ne15@0813'
+db = SQLAlchemy(app)
+
+
+class user(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[input_required(), length(
+        min=4, max=25)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[input_required(), length(
+        min=4, max=25)], render_kw={"placeholder": "password"})
+    submit = SubmitField("Register")
 
 
 @app.route('/')
@@ -8,5 +32,15 @@ def index():
     return render_template('index.html')
 
 
-if __name__ == "__main__":
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
     app.run(debug=True)
